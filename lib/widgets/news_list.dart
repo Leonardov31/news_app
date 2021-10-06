be4bded67444fd534cac6d74f6f520f6207372ba
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-10-06 11:06:48 -0300

    Create news_list.dart

---
+++ lib/widgets/news_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../viewmodels/news_article_view_model.dart';
+
+class NewsList extends StatelessWidget {
+  const NewsList({
+    Key? key,
+    required this.articles,
+    required this.onTap,
+  }) : super(key: key);
+
+  final List<NewsArticleViewModel> articles;
+  final Function(NewsArticleViewModel article) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: articles.length,
+      itemBuilder: (context, index) {
+        final article = articles[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 5.0),
+          child: ListTile(
+            onTap: () => onTap(article),
+            leading: SizedBox(
+              width: 100,
+              child: article.imageUrl != "Null"
+                  ? Image.network(articles[index].imageUrl)
+                  : Image.asset("assets/images/news-placeholder.png"),
+            ),
+            contentPadding: const EdgeInsets.all(10.0),
+            title: Text(article.title),
+          ),
+        );
+      },
+    );
+  }
+}
